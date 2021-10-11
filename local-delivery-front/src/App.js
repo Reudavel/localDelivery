d828e3b86bdb6a1e8915807f86e5fc9ab8334870
Raul <reudavel@gmail.com>
2021-10-10 21:33:56 -0500

    Step 2

--- local-delivery-front/src/App.js
+++ local-delivery-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reeload.
+          Edit <code>src/App.js</code> and save to rsdfsdfeeload.
         </p>
         <a
           className="App-link"
